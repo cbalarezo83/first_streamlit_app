9c69e1b1df8ba62d132ccf21fdf3cd66fb1909ba
cbalarezo83 <32489822+cbalarezo83@users.noreply.github.com>
2022-06-08 16:47:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list where fruit_name is not null")
+my_cur.execute("SELECT * from fruit_load_list where fruit_name <>'' ")
 #my_data_row = my_cur.fetchone()
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit list contains :")
