0601c626ffc75c46caf6fe3817d6424089713424
cbalarezo83 <32489822+cbalarezo83@users.noreply.github.com>
2022-06-08 16:54:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
-
 import streamlit;
+import pandas;
+import requests;
+import snowflake.connector 
 
 streamlit.title('My parents new healthy diner');
 streamlit.header('Breakfast Menu')
@@ -9,7 +11,7 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
-import pandas;
+#import pandas;
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -17,7 +19,8 @@ fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-import requests;
+#import requests;
+
 streamlit.header('Fruityvice Fruit Advice !')
 fruit_choice = streamlit.text_input('What fruit dude?','Kiwi')
 streamlit.write('User entered',fruit_choice)
@@ -30,7 +33,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
-import snowflake.connector 
+#import snowflake.connector 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
